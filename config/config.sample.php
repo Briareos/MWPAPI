c4a818b9b0549268fb86509feec56f379a9816b8
Milos Colakovic <milos.colakovic@prelovac.com>
2013-03-04 10:46:09 +0100

    Remove placeholders

--- config/config.sample.php
+++ config/config.sample.php
@@ -1,9 +1,9 @@
 <?php
 
 return array(
-    'db_host' => 'db_host_placeholder',
-    'db_name' => 'db_name_placeholder',
-    'db_user' => 'db_user_placeholder',
-    'db_password' => 'db_password_placeholder',
-    'db_table_prefix' => 'db_table_prefix_placeholder',
+    'db_host' => '',
+    'db_name' => '',
+    'db_user' => '',
+    'db_password' => '',
+    'db_table_prefix' => '',
 );
